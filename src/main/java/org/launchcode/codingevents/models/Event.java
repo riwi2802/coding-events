87361ce32b47c0cbaf4f1a677f2d690aebb88524
Richard WIlliams <riwi2802@colorado.edu>
2021-11-03 13:00:42 -0400

    end of chapter 12.3

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,4 +1,65 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
+
+    private String name;
+    private String description;
+
+    private int id;
+    private static int nextId = 1;
+
+
+    public Event(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.id = nextId;
+        nextId++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + description + " " + id;
+    }
+
+    //should typically add your own equals and has code methods.
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
